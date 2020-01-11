b16d1c4a4122785d9d0d22416428d90e8cb2f03b
Romeu Vieira <romeu.bizz@gmail.com>
2020-01-11 11:28:42 +0000

    Global font size scaling

--- init.el
+++ init.el
@@ -514,6 +514,27 @@ This command does not push text to `kill-ring'."
 (global-set-key (kbd "C-+") 'text-scale-increase)
 (global-set-key (kbd "C--") 'text-scale-decrease)
 
+(define-globalized-minor-mode
+  global-text-scale-mode
+  text-scale-mode
+  (lambda () (text-scale-mode 1)))
+
+(defun global-text-scale-adjust (inc) (interactive)
+       (text-scale-set 1)
+       (kill-local-variable 'text-scale-mode-amount)
+       (setq-default text-scale-mode-amount (+ text-scale-mode-amount inc))
+       (global-text-scale-mode 1)
+       )
+
+(global-set-key (kbd "M-0")
+		'(lambda () (interactive)
+		   (global-text-scale-adjust (- text-scale-mode-amount))
+		   (global-text-scale-mode -1)))
+(global-set-key (kbd "M-+")
+		'(lambda () (interactive) (global-text-scale-adjust 1)))
+(global-set-key (kbd "M--")
+		'(lambda () (interactive) (global-text-scale-adjust -1)))
+
 ;; highlight NOTE and TODO
 (setq-default fixme-modes '(c++-mode c-mode emacs-lisp-mode))
 (make-face 'font-lock-fixme-face)
