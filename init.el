f3d7f03075c3af6ffb7625acd1ef3366ce64a994
Romeu Vieira <romeu.bizz@gmail.com>
2020-04-18 12:29:48 +0100

    Performance improvements for package-initialize

--- init.el
+++ init.el
@@ -30,7 +30,9 @@
   (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/") t)
   (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
   (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
-  (package-initialize))
+  (package-initialize)
+  (setq package-enable-at-startup nil)
+  )
 
 (setq load-prefer-newer t)
 
