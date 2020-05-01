b7332503567a09df767760a4b55d83ac4d615dd0
Romeu Vieira <romeu.bizz@gmail.com>
2020-05-01 23:31:15 +0100

    Add custom.el

--- custom.el
+++ custom.el
@@ -3,7 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ccls-executable "ccls" t)
+ '(ccls-executable "ccls")
  '(company-echo-delay 0.1 t)
  '(company-idle-delay 0.1)
  '(company-minimum-prefix-length 1)
@@ -17,18 +17,32 @@
  '(helm-imenu-lynx-style-map t t)
  '(helm-occur-use-ioccur-style-keys t)
  '(helm-semantic-lynx-style-map t t)
- '(lsp-auto-guess-root t t)
- '(lsp-diagnostic-package :none t)
+ '(lsp-auto-guess-root t)
+ '(lsp-diagnostic-package :none)
  '(lsp-enable-semantic-highlighting nil t)
- '(lsp-enable-snippet nil t)
+ '(lsp-enable-snippet nil)
+ '(lsp-log-io nil)
  '(lsp-prefer-flymake nil t)
- '(lsp-print-io nil t)
- '(lsp-print-performance nil t)
+ '(lsp-print-io nil)
+ '(lsp-print-performance nil)
  '(lsp-trace nil t)
  '(magit-auto-revert-mode nil)
  '(package-selected-packages
    '(visual-regexp-steroids use-package typescript-mode stickyfunc-enhance srefactor sr-speedbar smartparens smart-mode-line rust-mode rg restclient px popwin pcre2el parent-mode ox-gfm org-web-tools org-ref org-bullets multiple-cursors move-text magit-popup lua-mode lsp-ui latex-preview-pane iedit hungry-delete helm-gtags helm-ag gruvbox-theme graphql git-timemachine ghub+ ggtags function-args fringe-helper forge flycheck find-file-in-project exec-path-from-shell elpy disable-mouse diffview diff-hl csv-mode company-quickhelp company-lsp company-auctex cmake-mode clean-aindent-mode clang-format ccls browse-at-remote bison-mode avy auctex-latexmk))
- '(popwin:popup-window-height 0.5 t))
+ '(popwin:popup-window-height 0.5 t)
+ '(safe-local-variable-values
+   '((eval setq compile-command
+		   (concat "make -C " my-project-path " all"))
+	 (eval set
+		   (make-local-variable 'my-project-path)
+		   (file-name-directory
+			(let
+				((d
+				  (dir-locals-find-file ".")))
+			  (if
+				  (stringp d)
+				  d
+				(car d))))))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
