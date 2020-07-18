07586c0b38b5b5e4438ece6ebcbb498e5ea06970
Romeu Vieira <romeu.bizz@gmail.com>
2020-07-18 01:07:45 +0100

    custom.el

--- custom.el
+++ custom.el
@@ -3,14 +3,16 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ccls-executable "ccls" t)
+ '(ansi-color-names-vector
+   ["#3c3836" "#fb4933" "#b8bb26" "#fabd2f" "#83a598" "#d3869b" "#8ec07c" "#ebdbb2"])
+ '(ccls-executable "ccls")
  '(company-echo-delay 0.1 t)
  '(company-idle-delay 0.1)
  '(company-minimum-prefix-length 1)
  '(company-quickhelp-delay 0.8 t)
  '(company-tooltip-align-annotations t)
  '(custom-safe-themes
-   '("4cf9ed30ea575fb0ca3cff6ef34b1b87192965245776afa9e9e20c17d115f3fb" default))
+   '("a06658a45f043cd95549d6845454ad1c1d6e24a99271676ae56157619952394a" "939ea070fb0141cd035608b2baabc4bd50d8ecc86af8528df9d41f4d83664c6a" "aded61687237d1dff6325edb492bde536f40b048eab7246c61d5c6643c696b7f" "4cf9ed30ea575fb0ca3cff6ef34b1b87192965245776afa9e9e20c17d115f3fb" default))
  '(helm-apropos-function-list
    '(helm-def-source--emacs-commands helm-def-source--emacs-functions helm-def-source--emacs-variables helm-def-source--emacs-faces))
  '(helm-ff-lynx-style-map t)
@@ -26,9 +28,26 @@
  '(lsp-print-io nil t)
  '(lsp-print-performance nil t)
  '(lsp-trace nil t)
+ '(lsp-ui-doc-enable nil)
+ '(lsp-ui-doc-header t)
+ '(lsp-ui-flycheck-enable t t)
+ '(lsp-ui-imenu-enable t)
+ '(lsp-ui-imenu-kind-position 'top)
+ '(lsp-ui-peek-enable t)
+ '(lsp-ui-peek-fontify 'on-demand)
+ '(lsp-ui-peek-list-width 50)
+ '(lsp-ui-peek-peek-height 20)
+ '(lsp-ui-sideline-code-actions-prefix "" t)
+ '(lsp-ui-sideline-enable nil)
+ '(lsp-ui-sideline-ignore-duplicate t)
+ '(lsp-ui-sideline-show-code-actions t)
+ '(lsp-ui-sideline-show-diagnostics nil)
+ '(lsp-ui-sideline-show-hover t)
+ '(lsp-ui-sideline-show-symbol t)
  '(magit-auto-revert-mode nil)
  '(package-selected-packages
-   '(visual-regexp-steroids use-package typescript-mode stickyfunc-enhance srefactor sr-speedbar smartparens smart-mode-line rust-mode rg restclient px popwin pcre2el parent-mode ox-gfm org-web-tools org-ref org-bullets multiple-cursors move-text magit-popup lua-mode lsp-ui latex-preview-pane iedit hungry-delete helm-gtags helm-ag gruvbox-theme graphql git-timemachine ghub+ ggtags function-args fringe-helper forge flycheck find-file-in-project exec-path-from-shell elpy disable-mouse diffview diff-hl csv-mode company-quickhelp company-lsp company-auctex cmake-mode clean-aindent-mode clang-format ccls browse-at-remote bison-mode avy auctex-latexmk))
+   '(unicode-fonts all-the-icons diminish visual-regexp-steroids use-package typescript-mode stickyfunc-enhance srefactor sr-speedbar smartparens smart-mode-line rust-mode rg restclient px popwin pcre2el parent-mode ox-gfm org-web-tools org-ref org-bullets multiple-cursors move-text magit-popup lua-mode lsp-ui latex-preview-pane iedit hungry-delete helm-gtags helm-ag gruvbox-theme graphql git-timemachine ghub+ ggtags function-args fringe-helper forge flycheck find-file-in-project exec-path-from-shell elpy disable-mouse diffview diff-hl csv-mode company-quickhelp company-lsp company-auctex cmake-mode clean-aindent-mode clang-format ccls browse-at-remote bison-mode avy auctex-latexmk))
+ '(pdf-view-midnight-colors '("#fdf4c1" . "#32302f"))
  '(popwin:popup-window-height 0.5 t)
  '(safe-local-variable-values
    '((eval setq compile-command
