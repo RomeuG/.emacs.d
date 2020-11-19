0d9f03ca1b671c7f424b4513c4c3a459f5d859b4
Romeu Vieira <romeu.bizz@gmail.com>
2020-11-19 14:52:55 +0000

    Comment garbage collector stuff

--- init.el
+++ init.el
@@ -1,11 +1,7 @@
-;; garbage collector
-(setq gc-cons-threshold most-positive-fixnum ; 2^61 bytes
-      gc-cons-percentage 0.6)
-
-(add-hook 'emacs-startup-hook
-	  (lambda ()
-	    (setq gc-cons-threshold 16777216 ; 16mb
-		  gc-cons-percentage 0.1)))
+;; (add-hook 'emacs-startup-hook
+;; 	  (lambda ()
+;; 	    (setq gc-cons-threshold 16777216 ; 16mb
+;; 		  gc-cons-percentage 0.1)))
 
 ;; file-name-handler-list optimization
 (defvar my--file-name-handler-alist file-name-handler-alist)
